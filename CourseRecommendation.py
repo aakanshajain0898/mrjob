467ff38e0b5005ee277ed9d9e06e14653e20861c
Aakansha Jain <aakanshajain0898@gmail.com>
2022-08-28 09:36:58 +0530

    Add files via upload
    initial files

---
+++ CourseRecommendation.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding=utf-8
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+# from itertools import combinations
+# from math import sqrt
+ 
+class CourseRecommendation(MRJob):
+    """ 
+    The is to aggregate all rating data of a single job_post
+    The format is: job_post, (course_item_count, [(course_title,course_rating)...])
+    """
+    def group_by_course_rating(self, _, line):
+        """
+        The mapper output is:
+        abc bcd,4.7
+        xyz vxy,1.7
+        abc cde,2.7
+        xyz uvx,3.7
+        """
+        job_post ,course_title, course_rating = line.split('|')
+        yield job_post, (course_title, float(course_rating))
+ 
+    def count_ratings_freq(self, job_post, values):
+        """
+        The reducer output is:
+        abc ( [2,[[bcd,4.7],[cde,2.7]]])
+        xyz ( [2,[[vxy,1.7],[uvx,3.7]]])
+        """
+        course_count = 0
+        ratings = []
+        for course_title, course_rating in values:
+            course_count += 1
+            ratings.append((course_title, course_rating))
+ 
+        yield job_post, (course_count, ratings) 
+ 
+    def steps(self):
+        return [
+            MRStep(mapper=self.group_by_course_rating,
+                reducer=self.count_ratings_freq),
+        ]
+
+if __name__ =='__main__':
+    CourseRecommendation.run()                      
---
+++ PreprocessCourseRecommendationData.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import csv
+
+if __name__=='__main__':
+    courses=[]
+    job_posts=[]
+    with open('coursea_data-1.csv', 'r') as file:
+        csvreader = csv.reader(file)
+        header = next(csvreader)
+        for row in csvreader:
+            courses.append(row)         
+
+    with open('data_job_posts.csv', 'r') as file:
+        csvreader = csv.reader(file)
+        header = next(csvreader)
+        for row in csvreader:
+            row[0] = row[0].replace("\n", "\t")
+            job_posts.append(row)        
+ 
+    courses_hash=[]
+    for post in job_posts:
+        for course in courses:
+            course_item = course[1].split(" ")
+            if ([ele for ele in course_item if(ele in post[0])]) :
+                
+                courses_hash.append([post[0], course[1], course[4] ])  
+ 
+    with open('recommended_courses.csv','w+') as f:
+        for course in courses_hash:
+            f.write(course[0]+'|'+course[1]+'|'+course[2]+'\n')
+
+    print("PreProcessing Done ---------------------------------------------------------->")       
+     
